f31695caef1066fafc8cbf68ac3780218f434811
dlwlrma123 <alexfinhart@gmail.com>
2021-10-05 11:15:24 +0800

    surya: init: switch and update fp/desc to barbet (pixel 5a) Oct fp/desc * https://dumps.tadiphone.dev/dumps/google/barbet/-/blob/barbet-user-11-RD2A.211001.002-7644766-release-keys/system/system/build.prop
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -97,8 +97,8 @@ void set_device_props(const string fingerprint, const string description,
 void load_device_properties() {
     string hwname = GetProperty("ro.boot.hwname", "");
 
-    string fingerprint = "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys";
-    string description = "redfin-user 11 RQ3A.210905.001 7511028 release-keys";
+    string fingerprint = "google/barbet/barbet:11/RD2A.211001.002/7644766:user/release-keys";
+    string description = "barbet-user 11 RD2A.211001.002 7644766 release-keys";
 
     if (hwname == "surya") {
         set_device_props(fingerprint, description, "Poco", "surya", "POCO X3 NFC (surya)");
